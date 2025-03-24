5cfe703950cd85d093cccaf0b6a81266ba8a8cd6
Nazar Pugach <qwertyasd0933@gmail.com>
2025-03-24 11:11:31 +0200

    lab 5 + lab 6

---
+++ lab7/lab7pii/Program.cs
@@ -0,0 +1,48 @@
+﻿using Azure;
+using System;
+using Azure.AI.TextAnalytics;
+
+namespace Example
+{
+    class Program
+    {
+        // This example requires environment variables named "LANGUAGE_KEY" and "LANGUAGE_ENDPOINT"
+        static string languageKey = Environment.GetEnvironmentVariable("LANGUAGE_KEY");
+        static string languageEndpoint = Environment.GetEnvironmentVariable("LANGUAGE_ENDPOINT");
+
+        private static readonly AzureKeyCredential credentials = new AzureKeyCredential(languageKey);
+        private static readonly Uri endpoint = new Uri(languageEndpoint);
+
+        // Example method for detecting sensitive information (PII) from text 
+        static void RecognizePIIExample(TextAnalyticsClient client)
+        {
+            string document = "Call our office at 312-555-1234, or send an email to support@contoso.com.";
+
+            PiiEntityCollection entities = client.RecognizePiiEntities(document).Value;
+
+            Console.WriteLine($"Redacted Text: {entities.RedactedText}");
+            if (entities.Count > 0)
+            {
+                Console.WriteLine($"Recognized {entities.Count} PII entit{(entities.Count > 1 ? "ies" : "y")}:");
+                foreach (PiiEntity entity in entities)
+                {
+                    Console.WriteLine($"Text: {entity.Text}, Category: {entity.Category}, SubCategory: {entity.SubCategory}, Confidence score: {entity.ConfidenceScore}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No entities were found.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var client = new TextAnalyticsClient(endpoint, credentials);
+            RecognizePIIExample(client);
+
+            Console.Write("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+    }
+}
